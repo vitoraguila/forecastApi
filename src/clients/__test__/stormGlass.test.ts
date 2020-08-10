4864168e3a999761d62ee3353ee50848cc472edc
Vitor Aguila <vitor.aguila1@gmail.com>
2020-08-10 20:57:00 +0200

    nodejs configuration and some functional and unit tests

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,21 @@
+import { StormGlass } from "@src/clients/stormGlass";
+import axios from "axios";
+import stormGlassWeather3HoursFixture from "@test/fixtures/stormglass_weather_3_hours.json";
+import stormGlassNormalized3HoursFixture from "@test/fixtures/stormglass_normalized_response_3_hours.json";
+
+jest.mock("axios");
+
+describe("StormGlass client", () => {
+  it("should return the normalized forecast from the stormGlass service", async () => {
+    const lat = -33.384344;
+    const lng = 151.343434;
+
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
