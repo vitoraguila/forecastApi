cb712e624a5c7f7b508c7f3e85894abedb4669be
Vitor Aguila <vitor.aguila1@gmail.com>
2020-08-11 00:19:35 +0200

    implementing error handler

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -17,4 +17,56 @@ describe("StormGlass client", () => {
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
+
+  it("should exclude imcomplete data points", async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+    const incommpleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: "2020-04-26T00:00:00+00:00",
+        },
+      ],
+    };
+    mockedAxios.get.mockResolvedValue({ data: incommpleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual([]);
+  });
+
+  it("should get a generic error from StormGlass service when the request fail before reaching the service", async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({ message: "Network Error" });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      "Unexpected error when trying to communicate to StormGlass: Network Error"
+    );
+  });
+
+  it("should get a generic error from StormGlass service when the request fail before reaching the service", async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { error: ["Rate Limit reached"] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error {"error":["Rate Limit reached"]} Code: 429'
+    );
+  });
 });
