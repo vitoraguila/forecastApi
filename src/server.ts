86068dc98283b42d22cea7a463d19bed6df7b402
Vitor Aguila <vitor.aguila1@gmail.com>
2020-09-02 23:53:02 +0200

    create user service

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { ForecastController } from "./controllers/forecast";
 import { Application } from "express";
 import * as database from "@src/database";
 import { BeachesController } from "./controllers/beaches";
+import { UsersController } from "./controllers/users";
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
@@ -23,7 +24,12 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
+    const usersController = new UsersController();
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   private async databaseSetup(): Promise<void> {
@@ -36,5 +42,11 @@ export class SetupServer extends Server {
 
   public getApp(): Application {
     return this.app;
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info("Server listening of port:", this.port);
+    });
   }
 }
